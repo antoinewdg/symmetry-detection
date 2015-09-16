c34ce26d3264ef06dc8c952cf797407c79bd4004
AntoineWDG <antoinewendlinger@gmail.com>
2015-09-16 10:17:50 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv )
+{
+
+    Mat image;
+    image = imread( "../assets/img/image.jpg");
+
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    imshow("Display Image", image);
+
+    waitKey(0);
+
+    return 0;
+}
